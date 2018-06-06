fd0d2c52502e987df72eeaf6b67520d292d587f4
Jung-hwan <ahnmo@pagez.kr>
2018-06-06 23:20:25 +0900

    add

--- remove.js
+++ remove.js
@@ -1,52 +1,56 @@
 
 function deleteElements(selector) {
-    // in case the content script was injected after the page is partially loaded
-    doDelete(document.querySelectorAll(selector));
+  // in case the content script was injected after the page is partially loaded
+  doDelete(document.querySelectorAll(selector));
 
-    var mo = new MutationObserver(process);
-    mo.observe(document, {subtree:true, childList:true});
-    document.addEventListener('DOMContentLoaded', function() { mo.disconnect() });
+  var mo = new MutationObserver(process);
+  mo.observe(document, {subtree:true, childList:true});
+  document.addEventListener('DOMContentLoaded', function() { mo.disconnect() });
 
-    function process(mutations) {
-        for (var i = 0; i < mutations.length; i++) {
-            var nodes = mutations[i].addedNodes;
-            for (var j = 0; j < nodes.length; j++) {
-                var n = nodes[j];
-                if (n.nodeType != 1) // only process Node.ELEMENT_NODE
-                    continue;
-                doDelete(n.matches(selector) ? [n] : n.querySelectorAll(selector));
-            }
-        }
+  function process(mutations) {
+    for (var i = 0; i < mutations.length; i++) {
+      var nodes = mutations[i].addedNodes;
+      for (var j = 0; j < nodes.length; j++) {
+        var n = nodes[j];
+        if (n.nodeType != 1) // only process Node.ELEMENT_NODE
+        continue;
+        doDelete(n.matches(selector) ? [n] : n.querySelectorAll(selector));
+      }
     }
-    function doDelete(nodes) {
-        [].forEach.call(nodes, function(node) { 
-            node.remove() 
-            
-           chrome.runtime.sendMessage({site: window.location.href}, function(){
-            
-           });
-        });
-    }
+  }
+  function doDelete(nodes) {
+    [].forEach.call(nodes, function(node) {
+      node.remove()
+
+      chrome.runtime.sendMessage({site: window.location.href}, function(){
+
+      });
+    });
+  }
 }
 
 
 
 chrome.storage.sync.get('block', function(res){
-    if(res.block){
-      
-deleteElements('script[src="https://coin-hive.com/lib/coinhive.min.js"]','script[src="coinhive.min.js"]','script[src="https://coinhive.com/lib/coinhive.min.js"]')  
-    }else{
-        
-    }
+  if(res.block){
+
+    deleteElements('script[src="https://coin-hive.com/lib/coinhive.min.js"]','script[src="coinhive.min.js"]','script[src="https://coinhive.com/lib/coinhive.min.js"]')
+  }else{
+
+  }
 
 });
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-       chrome.storage.sync.get('block', function(res){
-            if(res.block){
-              
-deleteElements('script[src="https://coin-hive.com/lib/coinhive.min.js"]','script[src="coinhive.min.js"]','script[src="https://coinhive.com/lib/coinhive.min.js"]')  
+  chrome.storage.sync.get('block', function(res){
+    if(res.block) {
+      deleteElements('script[src="https://coin-hive.com/lib/coinhive.min.js"]','script[src="coinhive.min.js"]','script[src="https://coinhive.com/lib/coinhive.min.js"]')
+    }
+  });
+});
 
-                }
-
-        }); 
-      });
+// XXX: THIS
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+ if(sender.id == chrome.runtime.id && request.message == "AHNMO_DETECTED") {
+   console.log("IS511-DETECTED");
+ }
+});
