ad063e61e27fcf6c71ebc8d45a74aa7b3e81c9f9
Jung-hwan <ahnmo@pagez.kr>
2018-06-07 03:00:34 +0900

    bugfix again

--- bg.js
+++ bg.js
@@ -71,10 +71,12 @@ function CpuUsage() {
     }
   });
 }
+
 CpuUsage()
 setInterval(function(){
   CpuUsage()
 },10000)
+
 function block(blacklist) {
   console.log('Ready to block')
   chrome.storage.sync.get('block', function(res){
@@ -106,26 +108,11 @@ function block(blacklist) {
           });
           chrome.browserAction.onClicked.addListener(function(tab) {
             chrome.browserAction.getBadgeText({}, function(result) {
-              if(result == 'On'){
-                chrome.browserAction.setBadgeText({text:'Stats'});
-                chrome.browserAction.setPopup({
-                  popup: 'popup.html'
-                })
+              if(result == 'On'){ chrome.browserAction.setBadgeText({text:'Stats'}); chrome.browserAction.setPopup({ popup: 'popup.html' })
               }else if(result == 'Stats'){
-                chrome.storage.sync.set({'block': false}, function() {
-                  console.log('Turn Off');
-                  chrome.browserAction.setPopup({
-                    popup: ''
-                  })
-                });
-
+                chrome.storage.sync.set({'block': false}, function() { console.log('Turn Off'); chrome.browserAction.setPopup({popup: ''}) });
               }else if(result == 'Off'){
-                chrome.storage.sync.set({'block': true}, function() {
-                  console.log('Turn On');
-                  chrome.browserAction.setPopup({
-                    popup: ''
-                  })
-                });
+                chrome.storage.sync.set({'block': true}, function() { console.log('Turn On'); chrome.browserAction.setPopup({popup: ''}) });
 
               }
             });
@@ -135,100 +122,100 @@ function block(blacklist) {
           }
         }
 
-        chrome.storage.onChanged.addListener(function(ress){
-          if(ress.stat){
-            number++;
-            if(number == 10){
-              chrome.storage.sync.get('feedback', function(res){
-                console.log(res.feedback)
-                if(res.feedback == null || res.feedback == 'undefined'){
-                  var inf = {
-                    type: "basic",
-                    title: 'Enjoy miner-free browsing?',
-                    message: 'Please consider leaving some feedback on our extension. Enjoy your mining-free browsing🎉',
-                    priority: 1,
-                    'requireInteraction': true,
-                    iconUrl: 'chrome-extension://ccagdbjcbhmcdcbbknfebhhdbolnfimo/128logo.png'
-                  };
+chrome.storage.onChanged.addListener(function(ress){
+  if(ress.stat){
+    number++;
+    if(number == 10){
+      chrome.storage.sync.get('feedback', function(res){
+        console.log(res.feedback)
+        if(res.feedback == null || res.feedback == 'undefined'){
+          var inf = {
+            type: "basic",
+            title: 'Enjoy miner-free browsing?',
+            message: 'Please consider leaving some feedback on our extension. Enjoy your mining-free browsing🎉',
+            priority: 1,
+            'requireInteraction': true,
+            iconUrl: 'chrome-extension://ccagdbjcbhmcdcbbknfebhhdbolnfimo/128logo.png'
+          };
 
-                  chrome.notifications.create('review', inf);
-                  chrome.notifications.onClicked.addListener(function(res){
-                    if(res == "review"){
-                      chrome.notifications.clear("review")
-                      chrome.tabs.create({ url: 'https://chrome.google.com/webstore/detail/coin-hive-blocker/ccagdbjcbhmcdcbbknfebhhdbolnfimo/reviews' });
-                    }
-                    chrome.storage.sync.set({'feedback': true})
-                  })
-                }else{
-                  console.log("Already prompted")
-                }
-              });
+          chrome.notifications.create('review', inf);
+          chrome.notifications.onClicked.addListener(function(res){
+            if(res == "review"){
+              chrome.notifications.clear("review")
+              chrome.tabs.create({ url: 'https://chrome.google.com/webstore/detail/coin-hive-blocker/ccagdbjcbhmcdcbbknfebhhdbolnfimo/reviews' });
             }
-          }
-        })
-
-        function start(){
-          fetch('https://raw.githubusercontent.com/andreas0607/CoinHive-blocker/master/blacklist.json').then(function(response) {
-            console.log(response.ok)
-            if(!response.ok) {
-              throw new Error('Network response was not ok.');
-            }
-            return response.json()
-          }).then(block).catch(function(error) {
-            console.log(error.message)
-            //Retry in 10 sec
-            console.log('Fetching local')
-            fetch('blacklist.json').then(function(response) {
-              return response.json()
-            }).then(block)
+            chrome.storage.sync.set({'feedback': true})
           })
-
+        }else{
+          console.log("Already prompted")
         }
+      });
+    }
+  }
+})
+
+function start(){
+  fetch('https://raw.githubusercontent.com/andreas0607/CoinHive-blocker/master/blacklist.json').then(function(response) {
+    console.log(response.ok)
+    if(!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    return response.json()
+  }).then(block).catch(function(error) {
+    console.log(error.message)
+    //Retry in 10 sec
+    console.log('Fetching local')
+    fetch('blacklist.json').then(function(response) {
+      return response.json()
+    }).then(block)
+  })
+
+}
 
 
-        var callback = function deny(block) {
-          chrome.storage.sync.get('stat', function(res){
-            if(res.stat === undefined){var obj = {}
-            chrome.storage.sync.set({'stat': obj}, function() {});
-          }else{
-            var obj = res.stat;
-            chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs){
-              console.log(tabs[0].url)
-              var site = tabs[0].url;
-              if(!(site in obj)){
-                var l = site;
-                obj[l] = 1;
-                chrome.storage.sync.set({'stat': obj}, function() {});
+var callback = function deny(block) {
+  chrome.storage.sync.get('stat', function(res){
+    if(res.stat === undefined){var obj = {}
+    chrome.storage.sync.set({'stat': obj}, function() {});
+  }else{
+    var obj = res.stat;
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs){
+      console.log(tabs[0].url)
+      var site = tabs[0].url;
+      if(!(site in obj)){
+        var l = site;
+        obj[l] = 1;
+        chrome.storage.sync.set({'stat': obj}, function() {});
 
-              }else{
-                var l = site;
-                obj[l] = obj[l] + 1;
-                chrome.storage.sync.set({'stat': obj}, function() {});
-              }
-            });
-          }
-        });
-        chrome.tabs.sendMessage(block.tabId, {message: "AHNMO_DETECTED"}); // XXX: THIS
-        return {cancel: true};
+      }else{
+        var l = site;
+        obj[l] = obj[l] + 1;
+        chrome.storage.sync.set({'stat': obj}, function() {});
       }
-      chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-          chrome.storage.sync.get('stat', function(res){
-            var obj = res.stat
-            if(res.stat === null){
-              chrome.storage.sync.set({'stat': {}}, function() {});
-            }else{
-              if(!(request.site in obj)){
-                var l = request.site;
-                obj[l] = 1;
-                chrome.storage.sync.set({'stat': obj}, function() {});
+    });
+  }
+});
+chrome.tabs.sendMessage(block.tabId, {message: "AHNMO_DETECTED"}); // XXX: THIS
+return {cancel: true};
+}
+chrome.runtime.onMessage.addListener(
+function(request, sender, sendResponse) {
+  chrome.storage.sync.get('stat', function(res){
+    var obj = res.stat
+    if(res.stat === null){
+      chrome.storage.sync.set({'stat': {}}, function() {});
+    }else{
+      if(!(request.site in obj)){
+        var l = request.site;
+        obj[l] = 1;
+        chrome.storage.sync.set({'stat': obj}, function() {});
 
-              }else{
-                var l = request.site;
-                obj[l] = obj[l] + 1;
-                chrome.storage.sync.set({'stat': obj}, function() {});
-              }
-            }
-          });
-        })
-        start()
+      }else{
+        var l = request.site;
+        obj[l] = obj[l] + 1;
+        chrome.storage.sync.set({'stat': obj}, function() {});
+      }
+    }
+  });
+})
+start()
